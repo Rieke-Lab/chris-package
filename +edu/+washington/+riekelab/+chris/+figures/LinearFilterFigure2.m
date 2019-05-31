e11c022952ccf858a90f4e309a481020e4bca7d8
chrischen <cqchen@uw.edu>
2019-05-31 13:10:58 -0700

    cleaning

--- +edu/+washington/+riekelab/+chris/+figures/LinearFilterFigure2.m
+++
@@ -1,7 +0,0 @@
-classdef LinearFilterFigure2 < edu.washington.riekelab.turner.figures.LinearFilterFigure
-    methods
-        function obj = LinearFilterFigure2(ampDevice, frameMonitor, varargin)
-            obj@edu.washington.riekelab.turner.figures.LinearFilterFigure(ampDevice, frameMonitor, varargin{:});
-        end
-    end
-end
