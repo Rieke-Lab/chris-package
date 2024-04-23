4e982fd118ece0a8a0727cf3c2e840a1d05d9b09
Julian Freedland <jfreedl@outlook.com>
2024-04-23 12:38:08 -0700

    procs in rig F

---
+++ +edu/+washington/+riekelab/+chris/+protocols/EyeMovementTrajectoryWithSurround.m
@@ -0,0 +1,277 @@
+classdef EyeMovementTrajectoryWithSurround < edu.washington.riekelab.protocols.RiekeLabStageProtocol
+    
+    properties
+        preTime = 0 % ms
+        stimTime = 60000 % ms
+        tailTime = 0 % ms
+        imageName = '00152' %van hateren image names
+        patchMean = 'all'
+        apertureDiameter = 250 % um
+        annulusInnerDiameter = 300; %  um
+        annulusOuterDiameter = 800; % u
+        surroundScale=1;
+        randomSeed = 1 % for eye movement trajectory
+        D = 5; % Drift diffusion coefficient, in microns
+        backgroundScale = 0.1; % scale factor for background relative to image mean
+        onlineAnalysis = 'extracellular'
+        numberOfAverages = uint16(3) % number of epochs to queue
+        amp % Output amplifier
+    end
+    
+    properties (Hidden)
+        ampType
+        imageNameType = symphonyui.core.PropertyType('char', 'row', {'00152','00377','00405','00459','00657','01151','01154',...
+            '01192','01769','01829','02265','02281','02733','02999','03093',...
+            '03347','03447','03584','03758','03760'})
+        patchMeanType = symphonyui.core.PropertyType('char', 'row', {'all','negative','positive'})
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'exc', 'inh'})
+        backgroundIntensity
+        imageMatrix
+        imageMeanIntensity
+        currentStimSet
+        currentImageSet
+        xTraj
+        yTraj
+        timeTraj
+        surroundTag
+        p0
+        xTraj_save
+        yTraj_save
+    end
+    
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+        
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            obj.showFigure('edu.washington.riekelab.turner.figures.MeanResponseFigure',...
+                obj.rig.getDevice(obj.amp),'recordingType',obj.onlineAnalysis, 'groupBy',{'surroundTag'});
+            obj.showFigure('edu.washington.riekelab.turner.figures.FrameTimingFigure',...
+                obj.rig.getDevice('Stage'), obj.rig.getDevice('Frame Monitor'));
+            
+            % get current image and stim (library) set:
+            resourcesDir = 'C:\Users\Public\Documents\turner-package\resources\';
+            obj.currentImageSet = '/VHsubsample_20160105';
+            obj.currentStimSet = 'SaccadeLocationsLibrary_20171011';
+            load([resourcesDir,obj.currentStimSet,'.mat']);
+            fieldName = ['imk', obj.imageName];
+            
+            %load appropriate image...
+            obj.currentStimSet = '/VHsubsample_20160105';
+            fileId=fopen([resourcesDir, obj.currentImageSet, '/imk', obj.imageName,'.iml'],'rb','ieee-be');
+            img = fread(fileId, [1536,1024], 'uint16');
+            img = double(img);
+            img = (img./max(img(:))); %rescale s.t. brightest point is maximum monitor level
+            img = img.*255; %rescale s.t. brightest point is maximum monitor level
+            obj.imageMatrix = uint8(img');
+            
+            %1) restrict to desired patch mean luminance:
+            imageMean = imageData.(fieldName).imageMean;
+            obj.backgroundIntensity = imageMean * obj.backgroundScale;%set the mean to the mean over the image with scale factor
+            locationMean = imageData.(fieldName).patchMean;
+            
+            if strcmp(obj.patchMean,'all')
+                inds = 1:length(locationMean);
+            elseif strcmp(obj.patchMean,'positive')
+                inds = find((locationMean-imageMean) > 0);
+            elseif strcmp(obj.patchMean,'negative')
+                inds = find((locationMean-imageMean) <= 0);
+            end
+            obj.imageMeanIntensity=imageMean;
+            rng(obj.randomSeed); %set random seed for fixation draw
+            drawInd = randsample(inds,1);
+            obj.p0(1) = imageData.(fieldName).location(drawInd,1);
+            obj.p0(2) = imageData.(fieldName).location(drawInd,2);
+            
+            %size of the stimulus on the prep:
+            stimSize = obj.rig.getDevice('Stage').getCanvasSize() .* obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'); %um
+            %scalingFactor: 3.3 for 1 arcmin/VH pixel (like DOVES), based
+            %on 198 um/degree on monkey retina
+            
+            % make eye movement trajectory.
+            rng(obj.randomSeed); %set random seed for fixation draw
+            noFrames = obj.rig.getDevice('Stage').getMonitorRefreshRate() * (obj.stimTime/1e3);
+            noFrames_perLoop = noFrames/2;
+            
+            %generate random walk out
+            tempX_1 = obj.D .* randn(1,round(noFrames_perLoop/2));
+            tempY_1 = obj.D .* randn(1,round(noFrames_perLoop/2));
+            %hold off first step to subtract later
+            tempX_1_a = tempX_1(2:end);
+            tempY_1_b = tempY_1(2:end);
+            %randomize walk back
+            tempX_2 = [-tempX_1_a(randperm(length(tempX_1_a))), -tempX_1(1)];
+            tempY_2 = [-tempY_1_b(randperm(length(tempY_1_b))), -tempY_1(1)];
+            
+            %cumulative sum, flip to start at 0
+            obj.xTraj = fliplr(cumsum([tempX_1, tempX_2]));
+            obj.yTraj = fliplr(cumsum([tempY_1, tempY_2]));
+            
+            obj.xTraj = [obj.xTraj, obj.xTraj]; %do the loop twice
+            obj.yTraj = [obj.yTraj, obj.yTraj]; %do the loop twice
+            
+            obj.xTraj_save = obj.xTraj; %still in VH coordinates
+            obj.yTraj_save = obj.yTraj;
+            
+            obj.timeTraj = (0:(length(obj.xTraj)-1)) ./...
+                obj.rig.getDevice('Stage').getMonitorRefreshRate(); %sec
+            
+            %need to make eye trajectories for PRESENTATION relative to the center of the image and
+            %flip them across the x axis: to shift scene right, move
+            %position left, same for y axis
+            obj.xTraj = -(obj.xTraj); %units=VH pixels
+            obj.yTraj = -(obj.yTraj);
+            %also scale them to canvas pixels
+            %canvasPix = (um) / (um/canvasPix)
+            obj.xTraj = obj.xTraj ./obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel');
+            obj.yTraj = obj.yTraj ./obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel');
+        end
+        
+        function p = createPresentation(obj)
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+            
+            apertureDiameterPix = obj.rig.getDevice('Stage').um2pix(obj.apertureDiameter);
+            annulusInnerDiameterPix = obj.rig.getDevice('Stage').um2pix(obj.annulusInnerDiameter);
+            annulusOuterDiameterPix = obj.rig.getDevice('Stage').um2pix(obj.annulusOuterDiameter);
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
+            p.setBackgroundColor(obj.backgroundIntensity);
+            
+            scene = stage.builtin.stimuli.Image(obj.imageMatrix);
+            %scale up image for canvas. Now image pixels and canvas pixels
+            %are the same size. Also image size is in rows (y), cols (x)
+            %but stage sizes are in x, y
+            %canvasPix = (VHpix) * (um/VHpix)/(um/canvasPix)
+            scene.size = [size(obj.imageMatrix,2) * (3.3)/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'),...
+                size(obj.imageMatrix,1) * (3.3)/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel')];
+            
+            tempY = obj.p0(2); %swap row/column for y/x
+            tempX = obj.p0(1);
+            %translate about center, shift to (0,0)
+            translatedX = -(tempX - 1536/2);
+            translatedY = (tempY - 1024/2);
+            translatedLocation = [translatedX, translatedY];
+            %scale to canvas pixels
+            translatedLocation = translatedLocation .* (3.3)/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel');
+            %re-center on canvas center
+            translatedLocation = canvasSize/2 + translatedLocation;
+            scene.position = translatedLocation;
+            
+            % Use linear interpolation when scaling the image.
+            scene.setMinFunction(GL.LINEAR);
+            scene.setMagFunction(GL.LINEAR);
+            
+            %apply eye trajectories to move image around
+            scenePosition = stage.builtin.controllers.PropertyController(scene,...
+                'position', @(state)getScenePosition(obj, state.time - obj.preTime/1e3, translatedLocation));
+            
+            function p = getScenePosition(obj, time, p0)
+                if time < 0
+                    p = p0;
+                elseif time > obj.timeTraj(end) %out of eye trajectory, hang on last frame
+                    p(1) = p0(1) + obj.xTraj(end);
+                    p(2) = p0(2) + obj.yTraj(end);
+                else %within eye trajectory and stim time
+                    dx = interp1(obj.timeTraj,obj.xTraj,time);
+                    dy = interp1(obj.timeTraj,obj.yTraj,time);
+                    p(1) = p0(1) + dx;
+                    p(2) = p0(2) + dy;
+                end
+            end
+            
+            p.addStimulus(scene);
+            p.addController(scenePosition);
+            
+            sceneVisible = stage.builtin.controllers.PropertyController(scene, 'visible', ...
+                @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+            p.addController(sceneVisible);
+            
+            if (obj.apertureDiameter > 0) %% Create aperture
+                aperture = stage.builtin.stimuli.Rectangle();
+                aperture.position = canvasSize/2;
+                %                 aperture.color = obj.backgroundIntensity;
+                aperture.color=0;
+                aperture.size = 2.*[max(canvasSize) max(canvasSize)];
+                mask = stage.core.Mask.createCircularAperture(apertureDiameterPix/(2*max(canvasSize)), 1024); %circular aperture
+                aperture.setMask(mask);
+                p.addStimulus(aperture); %add aperture
+            end
+            
+            if strcmp(obj.surroundTag,'none')
+                
+            else
+                %make annulus in surround
+                rect = stage.builtin.stimuli.Rectangle();
+                rect.position = canvasSize/2;
+                rect.size = [max(canvasSize) max(canvasSize)];
+                rect.color = obj.imageMeanIntensity*obj.surroundScale;
+                
+                distanceMatrix = createDistanceMatrix(1024);
+                annulus = uint8((distanceMatrix < annulusOuterDiameterPix/max(canvasSize) & ...
+                    distanceMatrix > annulusInnerDiameterPix/max(canvasSize)) * 255);
+                mask = stage.core.Mask(annulus);
+                
+                rect.setMask(mask);
+                p.addStimulus(rect);
+                rectVisible = stage.builtin.controllers.PropertyController(rect, 'visible', ...
+                    @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+                p.addController(rectVisible);
+            end
+            function m = createDistanceMatrix(size)
+                step = 2 / (size - 1);
+                [xx, yy] = meshgrid(-1:step:1, -1:step:1);
+                m = sqrt(xx.^2 + yy.^2);
+            end
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj, epoch);
+            
+            device = obj.rig.getDevice(obj.amp);
+            duration = (obj.preTime + obj.stimTime + obj.tailTime) / 1e3;
+            epoch.addDirectCurrentStimulus(device, device.background, duration, obj.sampleRate);
+            epoch.addResponse(device);
+            
+            surroundInd = mod(obj.numEpochsCompleted,2);
+            if surroundInd == 1
+                obj.surroundTag = 'imageMean';
+            elseif surroundInd == 0
+                obj.surroundTag = 'none';
+            end
+            
+            epoch.addParameter('backgroundIntensity', obj.backgroundIntensity);
+            epoch.addParameter('randomSeed', obj.randomSeed);
+            epoch.addParameter('currentStimSet',obj.currentStimSet);
+            epoch.addParameter('currentImageSet',obj.currentImageSet);
+            epoch.addParameter('xTraj',obj.xTraj_save);
+            epoch.addParameter('yTraj',obj.yTraj_save);
+            epoch.addParameter('surroundTag', obj.surroundTag);
+            epoch.addParameter('imageMeanIntensity', obj.imageMeanIntensity);
+        end
+        
+        % %         %same presentation each epoch in a run. Replay.
+        % %         function controllerDidStartHardware(obj)
+        % %             controllerDidStartHardware@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+        % %             if (obj.numEpochsCompleted >= 1) && (obj.numEpochsCompleted < obj.numberOfAverages)
+        % %                 obj.rig.getDevice('Stage').replay
+        % %             else
+        % %                 obj.rig.getDevice('Stage').play(obj.createPresentation());
+        % %             end
+        % %         end
+        
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages*2;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages*2;
+        end
+        
+    end
+    
+end
--- +edu/+washington/+riekelab/+chris/+protocols/variableMeanFlash.m
+++ +edu/+washington/+riekelab/+chris/+protocols/variableMeanFlash.m
@@ -7,6 +7,7 @@ classdef variableMeanFlash < edu.washington.riekelab.protocols.RiekeLabStageProt
         stimTime = 200                  % Spot duration (ms)
         tailTime = 400                  % Spot trailing duration (ms)
         spotDiameter = 2000              % Spot diameter size (um)
+        apertureDiameter=300
         flashContrast = [ -0.04 -0.08 -0.16 -0.02 0 0.02 0.04 0.08 0.16 ]
         meanIntensity = [0.03 0.06 0.12 0.18 0.24 0.36 0.54]       % Background light intensity (0-1)
         numberOfAverages = uint16(10)    % Number of epochs
@@ -37,29 +38,9 @@ classdef variableMeanFlash < edu.washington.riekelab.protocols.RiekeLabStageProt
         end
         
         function prepareRun(obj)
-            prepareRun@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);   
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
             % add the progress bar.
             obj.showFigure('edu.washington.riekelab.figures.FrameTimingFigure', obj.rig.getDevice('Stage'), obj.rig.getDevice('Frame Monitor'));
-        end
-        
-        function p = createPresentation(obj)
-            device = obj.rig.getDevice('Stage');
-            canvasSize = device.getCanvasSize();
-            
-            spotDiameterPix = device.um2pix(obj.spotDiameter);
-            
-            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
-            p.setBackgroundColor(obj.currentMean);
-            
-            spot = stage.builtin.stimuli.Ellipse();
-            spot.color = obj.currentMean(1+obj.currentContrast);
-            spot.radiusX = spotDiameterPix/2;
-            spot.radiusY = spotDiameterPix/2;
-            spot.position = canvasSize/2;
-            p.addStimulus(spot); 
-            spotVisible = stage.builtin.controllers.PropertyController(spot, 'visible', ...
-                @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
-            p.addController(spotVisible);
         end
         
         function prepareEpoch(obj, epoch)
@@ -72,10 +53,46 @@ classdef variableMeanFlash < edu.washington.riekelab.protocols.RiekeLabStageProt
             obj.currentMean=obj.meanIntensity(randi(length(obj.meanIntensity)));
             epoch.addDirectCurrentStimulus(device, device.background, duration, obj.sampleRate);
             epoch.addResponse(device);
-            epoch.addParameter('currentMean'.obj.currentMean)
+            epoch.addParameter('currentMean', obj.currentMean)
             epoch.addParameter('currentContrast',obj.currentContrast)
         end
         
+        
+        function p = createPresentation(obj)
+            try
+                device = obj.rig.getDevice('Stage');
+                canvasSize = device.getCanvasSize();
+                
+                spotDiameterPix = device.um2pix(obj.spotDiameter);
+                apertureDiameterPix = obj.rig.getDevice('Stage').um2pix(obj.apertureDiameter);
+
+                p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
+                p.setBackgroundColor(obj.currentMean);
+                
+                spot = stage.builtin.stimuli.Ellipse();
+                spot.color = obj.currentMean*(1+obj.currentContrast);
+                spot.radiusX = spotDiameterPix/2;
+                spot.radiusY = spotDiameterPix/2;
+                spot.position = canvasSize/2;
+                p.addStimulus(spot);
+                spotVisible = stage.builtin.controllers.PropertyController(spot, 'visible', ...
+                    @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+                p.addController(spotVisible);
+                if (obj.apertureDiameter > 0) %% Create aperture
+                    aperture = stage.builtin.stimuli.Rectangle();
+                    aperture.position = canvasSize/2;
+                    aperture.color = 0;
+                    aperture.size = [max(canvasSize) max(canvasSize)];
+                    mask = stage.core.Mask.createCircularAperture(apertureDiameterPix/max(canvasSize), 1024); %circular aperture
+                    aperture.setMask(mask);
+                    p.addStimulus(aperture); %add aperture
+                end
+            catch ME
+                disp(getReport(ME));
+            end
+        end
+        
+
         function prepareInterval(obj, interval)
             prepareInterval@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj, interval);
             
@@ -83,21 +100,13 @@ classdef variableMeanFlash < edu.washington.riekelab.protocols.RiekeLabStageProt
             interval.addDirectCurrentStimulus(device, device.background, obj.interpulseInterval, obj.sampleRate);
         end
         
-        function controllerDidStartHardware(obj)
-            controllerDidStartHardware@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
-            if obj.numEpochsPrepared == 1
-                obj.rig.getDevice('Stage').play(obj.createPresentation());
-            else
-                obj.rig.getDevice('Stage').replay();
-            end
-        end
         
         function tf = shouldContinuePreparingEpochs(obj)
-            tf = obj.numEpochsPrepared < obj.numberOfAverages*obj.meanIntensity*obj.flashContrast;
+            tf = obj.numEpochsPrepared < obj.numberOfAverages*numel(obj.meanIntensity)*numel(obj.flashContrast);
         end
         
         function tf = shouldContinueRun(obj)
-            tf = obj.numEpochsCompleted < obj.numberOfAverages*obj.meanIntensity*obj.flashContrast;
+            tf = obj.numEpochsCompleted < obj.numberOfAverages*numel(obj.meanIntensity)*numel(obj.flashContrast);
         end
         
     end
