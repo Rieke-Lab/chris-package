ac8dd1a1d4991e35b7df799fc0dfcc6015a6dc92
Chris Chen <cqchen@uw.edu>
2025-05-09 16:34:47 -0700

    Update SplitFieldRectangle.m

--- +edu/+washington/+riekelab/+chris/+inDev/SplitFieldRectangle.m
+++ +edu/+washington/+riekelab/+chris/+inDev/SplitFieldRectangle.m
@@ -1,6 +1,5 @@
 classdef SplitFieldRectangle < stage.core.Stimulus
     % A split-field rectangle stimulus with individually controllable left, middle, and right sections.
-    % This version uses a single initialization but still allows dynamic updates to section positions.
     
     properties
         position = [0, 0]           % Center position on the canvas [x, y] (pixels)
@@ -17,8 +16,12 @@ classdef SplitFieldRectangle < stage.core.Stimulus
 
     properties (Access = private)
         mask                        % Stimulus mask
-        vbo                         % Vertex buffer object
-        vao                         % Vertex array object
+        leftVbo                     % Left section vertex buffer object
+        middleVbo                   % Middle section vertex buffer object
+        rightVbo                    % Right section vertex buffer object
+        leftVao                     % Left section vertex array object
+        middleVao                   % Middle section vertex array object
+        rightVao                    % Right section vertex array object
     end
 
     methods
@@ -29,17 +32,53 @@ classdef SplitFieldRectangle < stage.core.Stimulus
                 obj.mask.init(canvas);
             end
 
-            % Initialize with placeholder data - will be updated in first draw
-            % 12 vertices for 3 quads (left, middle, right sections)
-            vertexData = zeros(1, 12 * 6); % Each vertex has 6 components (pos + tex)
-            obj.vbo = stage.core.gl.VertexBufferObject(canvas, GL.ARRAY_BUFFER, single(vertexData), GL.DYNAMIC_DRAW);
+            % Initialize the three sections of the split field
+            obj.initLeftSection(canvas);
+            obj.initMiddleSection(canvas);
+            obj.initRightSection(canvas);
+        end
+        
+        function initLeftSection(obj, canvas)
+            % Each vertex position is followed by a mask coordinate.
+            % Left section vertices (normalized coordinates)
+            vertexData = [-1  1  0  1,  0  1 ...
+                          -1 -1  0  1,  0  0 ...
+                           0  1  0  1,  1  1 ...
+                           0 -1  0  1,  1  0];
 
-            obj.vao = stage.core.gl.VertexArrayObject(canvas);
-            obj.vao.setAttribute(obj.vbo, 0, 4, GL.FLOAT, GL.FALSE, 6*4, 0);
-            obj.vao.setAttribute(obj.vbo, 1, 2, GL.FLOAT, GL.FALSE, 6*4, 4*4);
-            
-            % Initial update of vertex positions
-            obj.updateSectionPositions();
+            obj.leftVbo = stage.core.gl.VertexBufferObject(canvas, GL.ARRAY_BUFFER, single(vertexData), GL.STATIC_DRAW);
+
+            obj.leftVao = stage.core.gl.VertexArrayObject(canvas);
+            obj.leftVao.setAttribute(obj.leftVbo, 0, 4, GL.FLOAT, GL.FALSE, 6*4, 0);
+            obj.leftVao.setAttribute(obj.leftVbo, 1, 2, GL.FLOAT, GL.FALSE, 6*4, 4*4);
+        end
+        
+        function initMiddleSection(obj, canvas)
+            % Middle section vertices (normalized coordinates)
+            vertexData = [0  1  0  1,  0  1 ...
+                          0 -1  0  1,  0  0 ...
+                          0.2  1  0  1,  1  1 ...
+                          0.2 -1  0  1,  1  0];
+
+            obj.middleVbo = stage.core.gl.VertexBufferObject(canvas, GL.ARRAY_BUFFER, single(vertexData), GL.STATIC_DRAW);
+
+            obj.middleVao = stage.core.gl.VertexArrayObject(canvas);
+            obj.middleVao.setAttribute(obj.middleVbo, 0, 4, GL.FLOAT, GL.FALSE, 6*4, 0);
+            obj.middleVao.setAttribute(obj.middleVbo, 1, 2, GL.FLOAT, GL.FALSE, 6*4, 4*4);
+        end
+        
+        function initRightSection(obj, canvas)
+            % Right section vertices (normalized coordinates)
+            vertexData = [0.2  1  0  1,  0  1 ...
+                          0.2 -1  0  1,  0  0 ...
+                          1    1  0  1,  1  1 ...
+                          1   -1  0  1,  1  0];
+
+            obj.rightVbo = stage.core.gl.VertexBufferObject(canvas, GL.ARRAY_BUFFER, single(vertexData), GL.STATIC_DRAW);
+
+            obj.rightVao = stage.core.gl.VertexArrayObject(canvas);
+            obj.rightVao.setAttribute(obj.rightVbo, 0, 4, GL.FLOAT, GL.FALSE, 6*4, 0);
+            obj.rightVao.setAttribute(obj.rightVbo, 1, 2, GL.FLOAT, GL.FALSE, 6*4, 4*4);
         end
         
         function setMask(obj, mask)
@@ -56,29 +95,28 @@ classdef SplitFieldRectangle < stage.core.Stimulus
             leftEnd = -0.5 + (totalWidth/2 - halfGap) / totalWidth;
             rightStart = 0.5 - (totalWidth/2 - halfGap) / totalWidth;
             
-            % Combine all vertex data into one array
-            vertexData = [
-                % Left section (4 vertices with pos + tex)
-                -1      1  0  1,  0  1, ...
-                -1     -1  0  1,  0  0, ...
-                leftEnd  1  0  1,  1  1, ...
-                leftEnd -1  0  1,  1  0, ...
-                
-                % Middle section (4 vertices with pos + tex)
-                leftEnd     1  0  1,  0  1, ...
-                leftEnd    -1  0  1,  0  0, ...
-                rightStart  1  0  1,  1  1, ...
-                rightStart -1  0  1,  1  0, ...
-                
-                % Right section (4 vertices with pos + tex)
-                rightStart  1  0  1,  0  1, ...
-                rightStart -1  0  1,  0  0, ...
-                1           1  0  1,  1  1, ...
-                1          -1  0  1,  1  0  ...
-            ];
+            % Update left section vertices
+            leftVertexData = [-1    1  0  1,  0  1 ...
+                              -1   -1  0  1,  0  0 ...
+                              leftEnd  1  0  1,  1  1 ...
+                              leftEnd -1  0  1,  1  0];
             
-            % Update the vertex buffer object with all data at once
-            obj.vbo.uploadData(single(vertexData));
+            % Update middle section vertices
+            middleVertexData = [leftEnd     1  0  1,  0  1 ...
+                                leftEnd    -1  0  1,  0  0 ...
+                                rightStart  1  0  1,  1  1 ...
+                                rightStart -1  0  1,  1  0];
+            
+            % Update right section vertices
+            rightVertexData = [rightStart  1  0  1,  0  1 ...
+                               rightStart -1  0  1,  0  0 ...
+                               1           1  0  1,  1  1 ...
+                               1          -1  0  1,  1  0];
+            
+            % Update the vertex buffer objects
+            obj.leftVbo.uploadData(single(leftVertexData));
+            obj.middleVbo.uploadData(single(middleVertexData));
+            obj.rightVbo.uploadData(single(rightVertexData));
         end
     end
 
@@ -100,7 +138,7 @@ classdef SplitFieldRectangle < stage.core.Stimulus
             elseif length(leftC) == 3
                 leftC = [leftC, obj.leftOpacity];
             end
-            obj.canvas.drawArray(obj.vao, GL.TRIANGLE_STRIP, 0, 4, leftC, obj.mask);
+            obj.canvas.drawArray(obj.leftVao, GL.TRIANGLE_STRIP, 0, 4, leftC, obj.mask);
 
             % Draw middle section
             middleC = obj.middleColor;
@@ -109,7 +147,7 @@ classdef SplitFieldRectangle < stage.core.Stimulus
             elseif length(middleC) == 3
                 middleC = [middleC, obj.middleOpacity];
             end
-            obj.canvas.drawArray(obj.vao, GL.TRIANGLE_STRIP, 4, 4, middleC, obj.mask);
+            obj.canvas.drawArray(obj.middleVao, GL.TRIANGLE_STRIP, 0, 4, middleC, obj.mask);
 
             % Draw right section
             rightC = obj.rightColor;
@@ -118,7 +156,7 @@ classdef SplitFieldRectangle < stage.core.Stimulus
             elseif length(rightC) == 3
                 rightC = [rightC, obj.rightOpacity];
             end
-            obj.canvas.drawArray(obj.vao, GL.TRIANGLE_STRIP, 8, 4, rightC, obj.mask);
+            obj.canvas.drawArray(obj.rightVao, GL.TRIANGLE_STRIP, 0, 4, rightC, obj.mask);
 
             modelView.pop();
         end
